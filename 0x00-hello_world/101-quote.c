5f00f90f74fcc2e98beb824668848c37d36525e5
LabancaM <marc.ayi@imsp-uac.org>
2023-10-05 10:31:56 -0700

    variables and expansion

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, sizephrase;
+	char phrase[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	sizephrase = sizeof(phrase)/sizeof(phrase[0]);
+	for (i=0; i<sizephrase; i++)
+	{
+		putchar(phrase[i]);
+	}
+	putchar('\n');
+	return (0);
+}
