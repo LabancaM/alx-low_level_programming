4a787a4e959494a89cd6570237961452758879ac
LabancaM <marc.ayi@imsp-uac.org>
2023-10-06 20:07:03 -0700

    variable with if else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	digit = m % 10
+	digit = m % 10;
 	if (digit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	else if (n == 0)
