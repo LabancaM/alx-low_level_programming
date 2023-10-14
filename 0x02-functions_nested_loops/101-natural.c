32481d53d81a86b1ec8ed48c30ed0cb71c36efcb
LabancaM <marc.ayi@imsp-uac.org>
2023-10-14 07:41:24 -0700

    function nested

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -21,7 +21,7 @@ int main(void)
 int sum_natural(void)
 {
 	int n = 1024, i, sum = 0;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
@@ -29,5 +29,4 @@ int sum_natural(void)
 	}
 	printf("%d\n", sum);
 	return (sum);
-
 }
