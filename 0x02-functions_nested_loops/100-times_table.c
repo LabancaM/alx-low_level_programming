c6c207610a8dd3abb3bb39089d364497ca86fb99
LabancaM <marc.ayi@imsp-uac.org>
2023-10-14 06:17:30 -0700

    function nested

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * print_times_table - n*n  mulitipcation table
+ *
+ *@n: input integer
+ * Return: nothing
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			if (j == 0)
+			{
+				printf("0,");
+			}
+			else
+			{
+				if (i * j > 99)
+				{
+					printf(" %d", i*j);
+				}
+				else if (i * j > 9)
+				{
+					printf("  %d", i*j);
+				}
+				else
+					printf("   %d", i*j);
+				if (j > 0 && j < n)
+					printf(",");
+			}
+		}
+		printf("\n");
+	}
+}
