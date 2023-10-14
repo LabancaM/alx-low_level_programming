6764c25e74d0c516df1893d23651925bbd0cbb2e
LabancaM <marc.ayi@imsp-uac.org>
2023-10-14 07:03:04 -0700

    function nested

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * sum_natural - compute the addition of 3 and 5 multiple
+ *
+ * Return: result of 3 and 5 multiple addition
+ */
+int sum_natural(void)
+{
+	int n = 1024, i, sum = 0;
+	
+	for (i = 0; i < n; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (sum);
+}
