56caebfc09864e9a9a804059bc6c35fddd935023
LabancaM <marc.ayi@imsp-uac.org>
2023-10-16 17:21:07 -0700

    pointer array string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse of string
+ *
+ * @s: char input *s
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int len = 0, i;
+	char svg;
+
+	while (s[len] != '\0')
+		len++;
+	for (i = 0; i < len/2; i++)
+	{
+		svg = s[i];	
+		s[i] = s[len-i-1];
+		s[len-i-1] = svg;
+	}
+}
