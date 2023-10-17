d86b7e02c6be26f33e8c7cfb3e857ec7e58934bf
LabancaM <marc.ayi@imsp-uac.org>
2023-10-16 17:38:33 -0700

    pointer array string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2- print caracter of string
+ *
+ * @str: char input *str
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int len = 0;
+	char svg;
+
+	while (str[len] != '\0')
+	{
+		if (len % 2)
+		{
+			len++;
+			continue;
+		}
+		_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
+}
