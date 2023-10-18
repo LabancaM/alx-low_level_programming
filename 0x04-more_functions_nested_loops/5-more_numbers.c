8a8c09332fb4458bbbd799644a001af29f8e61cb
LabancaM <marc.ayi@imsp-uac.org>
2023-10-17 19:06:37 -0700

    more function

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - print digits exoeped 2 and 4
+ *
+ * Return: nothing 
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i < 10)
+				_putchar(i + 48);
+			else
+			{
+				_putchar(i / 10);
+				_putchar(i % 10);
+			}
+		}
+		_putchar('\n');
+	}
+}
