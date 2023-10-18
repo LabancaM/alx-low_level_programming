934a9984afdc7275741cc6001d3d2bf329c32dcb
LabancaM <marc.ayi@imsp-uac.org>
2023-10-17 18:06:45 -0700

    more function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - check if integer is a digit
+ *
+ * Return: nothing 
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i == 48; i < 58; i++)
+		_putchar(i);
+	_putchar('\n');
+}
