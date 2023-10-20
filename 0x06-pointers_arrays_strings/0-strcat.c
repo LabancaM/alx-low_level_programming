9ba6e63e72196cdfd6c082a5e6df6e29d4a33781
LabancaM <marc.ayi@imsp-uac.org>
2023-10-20 07:31:41 -0700

    pointer array  string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two stings
+ *
+ * @dest: destination input
+ * @src: source input
+ * Return: concatenate string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0, k, l;
+
+	while (src[i] != '\0')
+		i++;
+	while (dest[j] != '\0')
+		j++;
+	for (l = j, k = 0; l < i + j, k < i; l++, k++)
+	{
+		dest[l] = src[k];
+	}
+	return (dest);
+}
