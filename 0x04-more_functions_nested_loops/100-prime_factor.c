e4fd6ac6194a5a2d5a25183bc11ada94e5faea11
LabancaM <marc.ayi@imsp-uac.org>
2023-10-20 17:09:40 -0700

    more function

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long int i, n = 612852475143, m = 782849;
+
+	for (i = 3; i < m; i += 2)
+	{
+		while (n % i == 0 && n != i)
+		{
+			n /= i;
+		}	
+	}
+	printf("%lu\n", n);
+	return (0);
+}
