f17f0fecfa1df56a83df61dddcd8a6a0c4eb83af
LabancaM <marc.ayi@imsp-uac.org>
2023-10-20 19:14:51 -0700

    more pointer and array

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy string to other stings
+ *
+ * @dest: destination input
+ * @src: source input
+ * @n: n copy caracter
+ * Return: copy string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j = 0, l;
+
+	while (src[i] != '\0')
+		i++;
+	while (dest[j] != '\0')
+		j++;
+	if (n > j)
+		n = j;
+	for (l = 0; l < n; l++)
+	{
+		dest[l] = src[l];
+	}
+	return (dest);
+}
