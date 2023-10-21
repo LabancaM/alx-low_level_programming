df5d75a988cb64078a379c2eed12830ff67a3cc6
LabancaM <marc.ayi@imsp-uac.org>
2023-10-20 19:50:23 -0700

    more pointer and array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array value
+ *
+ * @a: array input
+ * @n: array size 
+ * Return: nothing 
+ */
+void reverse_array(int *a, int n)
+{
+	int i, k;
+	for (i = 0; i < n/ 2; i++)
+	{
+		k = *(a + i);
+		*(a + i) = *(a + n - i - 1);
+		*(a + n - i - 1) = k;
+	}	
+
+}
