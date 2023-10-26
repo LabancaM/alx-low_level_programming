2463bbdaded8d350ee39c6c5c40dcbf5f49b0a87
LabancaM <marc.ayi@imsp-uac.org>
2023-10-26 04:41:31 -0700

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,6 +9,7 @@
 int str_length(char *s)
 {
 	int n = 0;
+
 	if (*s != '\0')
 	{
 		n += 1;
@@ -23,13 +24,14 @@ int str_length(char *s)
  * @s: string to check
  * @n: length ok string
  * @k: integer input
+ * Return: 1 if ispalindrome other wise 0
  */
 int palindrome(char *s, int n, int k)
 {
-	if (s[k] != s[n-k-1])
-		return 0;
+	if (s[k] != s[n - k - 1])
+		return (0);
 	if (k == n / 2)
-		return 1;
+		return (1);
 	return (palindrome(s, n, ++k));
 }
 /**
@@ -41,7 +43,8 @@ int palindrome(char *s, int n, int k)
 int is_palindrome(char *s)
 {
 	int n = str_length(s);
+
 	if (n == 0)
-		return 0;
+		return (0);
 	return (palindrome(s, n, 0));
 }
