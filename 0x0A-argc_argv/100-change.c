d6301dabc39ce9baf9fa6ddc5ef555529f8f7f59
LabancaM <marc.ayi@imsp-uac.org>
2023-10-27 04:41:51 -0700

    argc, argv

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: number of argument in command line
+ * @argv: array of arguments
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int i, sum = 0, j, value[] = {25, 10, 5, 2, 1};
+
+	if (argc > 2 || argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	i = atoi(argv[1]);
+	if (i > 1)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			while (i >= value[j])
+			{
+				sum++;
+				i -= value[j];
+			}
+		}
+	}
+	printf("%d\n", sum);
+    	return (0);
+}
