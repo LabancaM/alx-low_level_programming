5f6723928eba2e80d4ad3eae86e0ba30d9de277d
LabancaM <marc.ayi@imsp-uac.org>
2023-10-27 04:05:23 -0700

    argc, argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point 
+ *
+ * @argc: number of argument in command line
+ * @argv: array of arguments
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int i, sum = 0;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			sum += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", sum);
+    	return (0);
+}
