d25fa9759a6775ffbc4d02a30e20b729c1d964cb
LabancaM <marc.ayi@imsp-uac.org>
2023-11-07 00:39:30 -0800

    pointer array  string

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,2 +1,17 @@
 #include "main.h"
 #include "dog.h"
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		print("NIL\n");
+	else
+		print("Name: %s", d->name);
+	if (d->age == NULL)
+		print("NIL\n");
+	else
+		print("Name: %s", d->name);
+	if (d->name == NULL)
+		print("NIL\n");
+	else
+		print("Name: %s", d->name);
+}
