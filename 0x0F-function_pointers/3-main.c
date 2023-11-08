6d9bdb2f2b2ab831dd874f95340408c955ceae6a
LabancaM <marc.ayi@imsp-uac.org>
2023-11-08 09:44:58 -0800

    function pointer

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ *
+ * @argc: number argument
+ * @argv: array of argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ( argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*' && argv[2][0] != '/' && argv[2][0] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	result = (*get_op_func(argv[2]))(a, b);	
+	printf("%d\n", result);
+	return (0);
+}
