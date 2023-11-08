4f12a523c8262d918c7138a2baeec68211d8fb63
LabancaM <marc.ayi@imsp-uac.org>
2023-11-08 09:43:37 -0800

    function pointer

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+#include "function_pointers.h"
+
+/**
+ * get_op_func - operation
+ * @s: carater
+ * 
+ * Return: result
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {{"+", op_add}, {"-", op_sub}, {"*", op_mul}, {"/", op_div},
+        {"%", op_mod}, {NULL, NULL}};
+	int i = 0;
+
+	while(i < 5)
+	{
+		if (s[0] == ops[i].op[0])
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * op_add - addition
+ * @a: integer a
+ * @b: integer b
+ *
+ * Return: integer
+ */
+int op_add(int a, int b)
+{
+	return(a + b);
+}
+/**
+ * op_sub - substraction
+ * @a: integer a
+ * @b: integer b
+ *
+ * Return: integer
+ */
+int op_sub(int a, int b)
+{
+	return(a - b);
+}
+/**
+ * op_mul - mul
+ * @a: integer a
+ * @b: integer b
+ *
+ * Return: integer
+ */
+int op_mul(int a, int b)
+{
+	return(a * b);
+}
+/**
+ * op_div - division
+ * @a: integer a
+ * @b: integer b
+ *
+ * Return: integer
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit (100);
+	}
+	return(a / b);
+}
+/**
+ * op_mod - module
+ * @a: integer a
+ * @b: integer b
+ *
+ * Return: integer
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit (100);
+	}
+	return(a % b);
+}
