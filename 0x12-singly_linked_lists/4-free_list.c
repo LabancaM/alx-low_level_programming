43d2d5b42cd9201a5b708743a9c1b4b21b2f0d3d
LabancaM <marc.ayi@imsp-uac.org>
2023-11-16 17:19:02 -0800

    linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - print element in list
+ * @head: list
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	free(head);
+}
