7348fa4719d4682f3c9e4c9696b028865d2a36c5
LabancaM <marc.ayi@imsp-uac.org>
2023-11-21 00:00:25 -0800

    more linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - print element in list
+ * @h: list
+ *
+ * Return: size_t
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+
+		printf("%d\n", h->n);
+		i++;
+		h = h->next;
+
+	}
+	return (i);
+}
