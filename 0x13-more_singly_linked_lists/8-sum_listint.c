e3ec7dee4c87a8293c152dfb86a3851cdf05d738
LabancaM <marc.ayi@imsp-uac.org>
2023-11-21 02:51:29 -0800

    more linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * sum_listint - print element in list
+ * @head: list of list
+ *
+ * Return: int
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while(head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
