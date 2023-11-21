837eea3e191bcc52d6a77726f6a42f90bf690bd3
LabancaM <marc.ayi@imsp-uac.org>
2023-11-21 02:43:17 -0800

    more linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,8 +18,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		if (i == index)
 			return (head);
-		head = head->next;
 		i++;
+		head = head->next;
 	}
 	return (NULL);
 }
