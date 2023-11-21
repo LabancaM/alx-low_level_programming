0736c563dffc8ca793d9e5be13ec275c11c542a8
LabancaM <marc.ayi@imsp-uac.org>
2023-11-21 02:39:35 -0800

    more linked list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - print element in list
+ * @head: list of list
+ * @index: integer
+ *
+ * Return: listint_t
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while(head != NULL)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
