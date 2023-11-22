0d5e5d8e3e51d1d71f23db55832cc34fa258da12
LabancaM <marc.ayi@imsp-uac.org>
2023-11-22 01:56:18 -0800

    more linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,8 +25,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		save = *head;
-		if (*head == NULL)
-			return (NULL);
 		*head = insert;
 		insert->next = save;
 		return (insert);
